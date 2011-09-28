29f7a5c8ebda688f711817154e949f3bb008186b
AbbyRaasch <alandowski@matcmadison.edu>
2011-09-27 19:08:33 -0500

    Field Validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,4 @@
 class Bookmark < ActiveRecord::Base
     validates :URL, :length => { :minimum => 10 }
-    validates :name, :length => { :maximum => 100 }
+    validates :name, :length => { :minimum => 1, :maximum => 100 }
 end
